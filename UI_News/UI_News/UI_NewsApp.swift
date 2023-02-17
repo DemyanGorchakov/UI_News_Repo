e3c2c83c9b540dbb0f13795f5983a610ef597301
Demyan Gorchakov <Damian456@mail.ru>
2023-02-17 21:56:22 +0300

    Получение новостей на SwiftUI

---
+++ UI_News/UI_News/UI_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_NewsApp.swift
+//  UI_News
+//
+//  Created by Демьян Горчаков on 17.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct UI_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
